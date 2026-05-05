c92ebca1e539f4216e379b5db7e35fae477f5632
Kurt <jfmanalac@up.edu.ph>
2026-05-06 01:21:45 +0800

    adjust for fabric-shim.server

--- service/index.js
+++ service/index.js
@@ -7,44 +7,45 @@ module.exports.contracts = [ArticleContract];
 async function main() {
     const serverOpts = {
         ccid: process.env.CHAINCODE_ID,
-        address: process.env.CHAINCODE_SERVER_ADDRESS || '0.0.0.0:9999',
-        tlsProps: {
-            key: process.env.CHAINCODE_TLS_KEY,
-            cert: process.env.CHAINCODE_TLS_CERT,
-            clientCACert: process.env.CHAINCODE_CLIENT_CA_CERT
-        }
+        address: process.env.CHAINCODE_SERVER_ADDRESS || '0.0.0.0:9999'
         
     };
     const shim = require('fabric-shim');
 
-    const Chaincode = class {
-        async Init(stub) {
-            // use the instantiate input arguments to decide initial chaincode state values
-
-            // save the initial states
-            await stub.putState(key, Buffer.from(aStringValue));
-
-            return shim.success(Buffer.from('Initialized Successfully!'));
+    var Chaincode = class {
+        Init(stub) {
+            return stub.putState('dummyKey', Buffer.from('dummyValue'))
+                .then(() => {
+                    console.info('Chaincode instantiation is successful');
+                    return shim.success();
+                }, () => {
+                    return shim.error();
+                });
         }
 
-        async Invoke(stub) {
-            // use the invoke input arguments to decide intended changes
+        Invoke(stub) {
+            console.info('Transaction ID: ' + stub.getTxID());
+            console.info(util.format('Args: %j', stub.getArgs()));
 
-            // retrieve existing chaincode states
-            let oldValue = await stub.getState(key);
+            let ret = stub.getFunctionAndParameters();
+            console.info('Calling function: ' + ret.fcn);
 
-            // calculate new state values and saves them
-            let newValue = oldValue + delta;
-            await stub.putState(key, Buffer.from(newValue));
-
-            return shim.success(Buffer.from(newValue.toString()));
+            return stub.getState('dummyKey')
+            .then((value) => {
+                if (value.toString() === 'dummyValue') {
+                    console.info(util.format('successfully retrieved value "%j" for the key "dummyKey"', value ));
+                    return shim.success();
+                } else {
+                    console.error('Failed to retrieve dummyKey or the retrieved value is not expected: ' + value);
+                    return shim.error();
+                }
+            });
         }
     };
 
     try {
-        shim.start(new Chaincode());
+        shim.server(new ArticleContract(), serverOpts);
         console.log(`Article Chaincode Server started on ${serverOpts.address}`);
-        await server.start();
     } catch (e) {
         console.error(`Error starting chaincode server: ${e.stack}`);
         process.exit(1);
