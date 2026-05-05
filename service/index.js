fb829da79e55d18796bfb3c7f7c8d895907e0cc7
Kurt <jfmanalac@up.edu.ph>
2026-05-06 01:29:38 +0800

    change index.js

--- service/index.js
+++ service/index.js
@@ -1,55 +1,55 @@
 'use strict';
 
-const shim = require('fabric-shim');
+// const shim = require('fabric-shim');
 const ArticleContract = require('./crud');
 module.exports.contracts = [ArticleContract];
 
-async function main() {
-    const serverOpts = {
-        ccid: process.env.CHAINCODE_ID,
-        address: process.env.CHAINCODE_SERVER_ADDRESS || '0.0.0.0:9999'
+// async function main() {
+//     const serverOpts = {
+//         ccid: process.env.CHAINCODE_ID,
+//         address: process.env.CHAINCODE_SERVER_ADDRESS || '0.0.0.0:9999'
         
-    };
-    const shim = require('fabric-shim');
+//     };
+//     const shim = require('fabric-shim');
 
-    var Chaincode = class {
-        Init(stub) {
-            return stub.putState('dummyKey', Buffer.from('dummyValue'))
-                .then(() => {
-                    console.info('Chaincode instantiation is successful');
-                    return shim.success();
-                }, () => {
-                    return shim.error();
-                });
-        }
+//     var Chaincode = class {
+//         Init(stub) {
+//             return stub.putState('dummyKey', Buffer.from('dummyValue'))
+//                 .then(() => {
+//                     console.info('Chaincode instantiation is successful');
+//                     return shim.success();
+//                 }, () => {
+//                     return shim.error();
+//                 });
+//         }
 
-        Invoke(stub) {
-            console.info('Transaction ID: ' + stub.getTxID());
-            console.info(util.format('Args: %j', stub.getArgs()));
+//         Invoke(stub) {
+//             console.info('Transaction ID: ' + stub.getTxID());
+//             console.info(util.format('Args: %j', stub.getArgs()));
 
-            let ret = stub.getFunctionAndParameters();
-            console.info('Calling function: ' + ret.fcn);
+//             let ret = stub.getFunctionAndParameters();
+//             console.info('Calling function: ' + ret.fcn);
 
-            return stub.getState('dummyKey')
-            .then((value) => {
-                if (value.toString() === 'dummyValue') {
-                    console.info(util.format('successfully retrieved value "%j" for the key "dummyKey"', value ));
-                    return shim.success();
-                } else {
-                    console.error('Failed to retrieve dummyKey or the retrieved value is not expected: ' + value);
-                    return shim.error();
-                }
-            });
-        }
-    };
+//             return stub.getState('dummyKey')
+//             .then((value) => {
+//                 if (value.toString() === 'dummyValue') {
+//                     console.info(util.format('successfully retrieved value "%j" for the key "dummyKey"', value ));
+//                     return shim.success();
+//                 } else {
+//                     console.error('Failed to retrieve dummyKey or the retrieved value is not expected: ' + value);
+//                     return shim.error();
+//                 }
+//             });
+//         }
+//     };
 
-    try {
-        shim.server(new ArticleContract(), serverOpts);
-        console.log(`Article Chaincode Server started on ${serverOpts.address}`);
-    } catch (e) {
-        console.error(`Error starting chaincode server: ${e.stack}`);
-        process.exit(1);
-    }
-}
+//     try {
+//         shim.server(new ArticleContract(), serverOpts);
+//         console.log(`Article Chaincode Server started on ${serverOpts.address}`);
+//     } catch (e) {
+//         console.error(`Error starting chaincode server: ${e.stack}`);
+//         process.exit(1);
+//     }
+// }
 
-main();
+// main();
